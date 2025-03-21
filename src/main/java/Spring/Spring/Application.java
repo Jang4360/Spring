7b7c61a39e4b630f6e301e096265ee0cb34b5a45
Jang4360 <jooyoon4360@gmail.com>
2025-03-21 11:59:21 +0900

    aspect 6.2.2

--- src/main/java/Spring/Spring/Application.java
+++ src/main/java/Spring/Spring/Application.java
@@ -1,14 +1,17 @@
 package Spring.Spring;
 
 import Spring.practice.aspects.Comment;
+import Spring.practice.aspects.Main;
 import Spring.practice.aspects.services.CommentService;
 import Spring.practice.aspects.ProjectConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.logging.Logger;
+
 //@SpringBootApplication
 public class Application {
-
+	private static Logger logger = Logger.getLogger(Main.class.getName());
 	public static void main(String[] args) {
 //		SpringApplication.run(Application.class, args);
 		var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
@@ -16,7 +19,9 @@ public class Application {
 		Comment comment = new Comment();
 		comment.setText("Demo comment");
 		comment.setAuthor("natash");
-		service.publishComment(comment);
+
+		String value = service.publishComment(comment);
+		logger.info(value);
 	}
 
 }
