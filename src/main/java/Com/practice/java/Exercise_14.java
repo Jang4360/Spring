69d428bc4d152c29a9611d8b8728b181d2a58dac
Jang4360 <jooyoon4360@gmail.com>
2025-07-19 22:52:37 +0900

    java grammer

--- src/main/java/Com/practice/java/Exercise_14.java
+++ src/main/java/Com/practice/java/Exercise_14.java
@@ -1,5 +1,7 @@
 package Com.practice.java;
 
+import java.util.*;
+
 class Print{
     public static String delimiter;
     public static void A() {
@@ -19,5 +21,15 @@ public class Exercise_14 {
     public static void main(String[] args) {
         Print.A();
         Print.B();
+
+        HashSet<String> set1 = new HashSet<>(Arrays.asList("1","2","3"));
+        HashSet<String> set2 = new HashSet<>(Arrays.asList("3","4","5","6","7"));
+        set1.addAll(Arrays.asList("8","9"));
+        set1.remove("3");
+
+        enum CoffeeType{
+
+        }
+        System.out.println(set1);
     }
 }
